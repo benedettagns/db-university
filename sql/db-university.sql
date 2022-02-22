0c68fbddf57a609b4d57478749ae0ed40f834892
benedettagns <benedetta.genise01@gmail.com>
2022-02-22 17:02:13 +0100

    completato

--- sql/db-university.sql
+++ sql/db-university.sql
@@ -20,6 +20,20 @@ WHERE `period` = 'I semestre'
 AND `year` = '1';
 
 --Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * 
+FROM `exams` 
+WHERE `hour` > '14:00:00';
+
 --Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees` 
+WHERE `level` = 'magistrale';
+
 --Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(id) 
+FROM `departments`;
+
 --Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(id) 
+FROM `teachers` 
+WHERE `phone` IS NULL;
