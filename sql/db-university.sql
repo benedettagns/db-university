8a97e5652619cb440ceb4deb7fa1625a37e7c0c9
benedettagns <benedetta.genise01@gmail.com>
2022-02-22 16:51:23 +0100

    corsi primo semestre

--- sql/db-university.sql
+++ sql/db-university.sql
@@ -14,7 +14,10 @@ FROM `students`
 WHERE `date_of_birth` <= '1992-01-01';
 
 --Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * 
+FROM `courses` 
+WHERE `period` = 'I semestre' 
+AND `year` = '1';
 
 --Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 --Selezionare tutti i corsi di laurea magistrale (38)
